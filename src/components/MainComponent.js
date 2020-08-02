b5d667df7a9b8e02c505e9cae1137eb81467ca90
Siddharth Tumre <siddharthtumre2@gmail.com>
2020-08-02 14:04:16 +0530

    deploying

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,49 +1,22 @@
 import React, {Component} from 'react'
 import DishDetail from "./DishDetailComponent"
+import {DISHES} from '../shared/dishes'
 
 class Main extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false,
-            dishes: []
+            dishes: DISHES
         }
     }
 
-    componentDidMount() {
-        fetch("http://starlord.hackerearth.com/recipe")
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                error: null,
-                isLoaded: true,
-                dishes: result
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
-
       render() {
-          const {error, isLoaded, dishes} = this.state;
-          if (error){
-            return <div>Error: {error.message}</div>;
-          } else if (!isLoaded) {
-            return <div>Loading...</div>;
-          }
-          else {
+          const {dishes} = this.state;
             return (
                 <div style={{margin: '80px 0px'}}>
                     <DishDetail dishes={dishes} />
                 </div>
             );
-          }
       }
 }
 
